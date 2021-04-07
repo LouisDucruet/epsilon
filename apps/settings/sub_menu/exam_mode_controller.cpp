b9a5a5bc961cfb1312a726a3f2f4dfbb5d6dd90e
LouisDucruet <lducruet74@gmail.com>
2021-04-07 09:43:33 +0200

    Delete error msg

--- apps/settings/sub_menu/exam_mode_controller.cpp
+++ apps/settings/sub_menu/exam_mode_controller.cpp
@@ -42,8 +42,8 @@ void ExamModeController::didEnterResponderChain(Responder * previousFirstRespond
     m_contentView.setMessages(deactivateMessages, k_numberOfDeactivationMessageLines);
   // --------------------- Please don't edit these lines ----------------------
   } else if (numberOfCautionLines() > 0) {
-    I18n::Message cautionMessages[] = {I18n::Message::ExamModeWarning1, I18n::Message::ExamModeWarning2, I18n::Message::ExamModeWarning3};
-    m_contentView.setMessages(cautionMessages, numberOfCautionLines());
+    //I18n::Message cautionMessages[] = {I18n::Message::ExamModeWarning1, I18n::Message::ExamModeWarning2, I18n::Message::ExamModeWarning3};
+    //m_contentView.setMessages(cautionMessages, numberOfCautionLines());
   }
   // --------------------------------------------------------------------------
 }
